531e75f260772898651ee357760f82cf63af985a
张炳驰 <1595759409@qq.com>
2022-07-03 11:36:39 +0800

    版本优化

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -471,13 +471,11 @@ class Index extends Base {
     }
 
     public function sitemaps() {
-        $lists = model('thread')->where('is_delete', 0)->order('id', 'desc')->select();
+        $lists = model('thread')->where('is_delete', 0)->limit(100)->order('id', 'desc')->select();
         foreach ($lists as $item) {
             $content = "http://show.lexiangtu.top/thread_views/{$item['article_id']}.html" . PHP_EOL;
             file_put_contents('sitemaps.txt', $content, FILE_APPEND);
         }
-
-        echo "导出完成";
         echo file_get_contents('sitemaps.txt');
     }
 
