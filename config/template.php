2f7c5e57ff0d406fb27d2250df96907315d8acc4
张炳驰 <1595759409@qq.com>
2022-07-09 00:51:44 +0800

    版本优化ladding

--- config/template.php
+++ config/template.php
@@ -36,6 +36,6 @@ return [
     'tpl_replace_string'       => [
         '__PUBLIC__' => APP_URL,
         '__THEME__' => APP_URL . '/static/' . APP_THEME,
-        '__MEDIA_URL__' => 'http://www.lexiangtu.top/media/images/',
+        '__MEDIA_URL__' => 'http://static.lexiangtu.top/media/images/',
     ]
 ];
